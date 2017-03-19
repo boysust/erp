3b273211e9a9fce211c103b682e1fb42086b8cd7
mldn <mldnqa@163.com>
2017-03-19 16:58:05 +0800

    Apply path

--- Hello.java
+++ Hello.java
@@ -1,15 +1,6 @@
 public  class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello GIT!!!");
-		System.out.println("hello git!!!");
-		System.out.println("HEllo git!!!");
-		System.out.println("sust");
-		System.out.println("测试合并");
-<<<<<<< HEAD
-		Admin admin = new Admin();
-		Emp emp = new Emp();
-=======
-		System.out.println("中午适合学习，因为大家都饿了");
->>>>>>> dev
-	}
+		System.out.println("创建补丁！");
+		System.out.println("要多运动，好好学习！");
+		}
 }
